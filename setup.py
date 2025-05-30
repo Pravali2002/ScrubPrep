a788a18299792a275593e11ccdbb3704fd960995
Pravalika Somisetty <pravalika@MacBookAir.attlocal.net>
2025-05-29 20:54:34 -0500

    structure of folder is added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="GenAI Project",
+    version="0.0.0",
+    author="Pravalika",
+    author_email= 'somisettypravalika@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
